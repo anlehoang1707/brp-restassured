3918bc7132a5fc47d785a5671bbf98cfce382bbf
leanprojectmanager <an.lehoang1707@gmail.com>
2025-07-03 13:24:07 +0700

    Finish Implement Level_04_Refactor_Level_03_With_POJO

---
+++ src/main/java/NewUserPOJO.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+
+@Data
+public class NewUserPOJO {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+}
